6fd291f0e753a2529b55f21d6b35d4acbb8d6b31
Mirko Pagliai <mirko.pagliai@gmail.com>
2016-06-11 16:18:56 +0200

    fixed

--- config/routes.php
+++ config/routes.php
@@ -23,7 +23,7 @@
 
 use Cake\Routing\Router;
 
-Router::plugin('Thumbs', ['path' => '/thumbs'], function ($routes) {
+Router::plugin('Thumbs', ['path' => '/thumbs'], function($routes) {
 	$routes->connect('/resize/*', ['controller' => 'Thumbs', 'action' => 'resize', 'plugin' => 'Thumbs'], ['_name' => 'resize']);
 	$routes->connect('/square/*', ['controller' => 'Thumbs', 'action' => 'square', 'plugin' => 'Thumbs'], ['_name' => 'square']);
 	$routes->connect('/thumb/*', ['controller' => 'Thumbs', 'action' => 'thumb', 'plugin' => 'Thumbs'], ['_name' => 'thumb']);
